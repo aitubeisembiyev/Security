e01c3ce3542fe3ef547e60c6a8cb1343eb10a52d
Beisembiyev Bakhytzhan <baha.applecity@gmail.com>
2022-05-16 12:31:51 +0600

    level 2

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
+//level 2.1
+require('dotenv').config();
+//
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 
+//level 2
+const encrypt = require("mongoose-encryption");
+//
+
 const app = express();
 
+//always use bodyparser before defining routes!!!
 app.use(bodyParser.urlencoded({extended: true}));
 
 const UserRoute = require('./routes/UserRoute')
@@ -37,37 +46,6 @@ app.get("/login", function(req, res){
 app.get("/register", function(req, res){
     res.render("register");
 });
-
-/*app.post("/register", function(req, res){
-    const newUser =  new User({
-        email: req.body.username,
-        password: req.body.password
-    });
-    newUser.save(function(err){
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("secrets");
-        }
-    });
-});
-
-app.post("/login", function(req, res){
-    const username = req.body.username;
-    const password = req.body.password;
-
-    User.findOne({email: username}, function(err, foundUser){
-        if (err) {
-            console.log(err);
-        } else {
-            if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render("secrets");
-                }
-            }
-        }
-    });
-});*/
 
 
 let port = process.env.PORT||3000;
--- models/UserModel.js
+++ models/UserModel.js
@@ -1,9 +1,18 @@
 const mongoose = require("mongoose");
-
+//level 2
+const encrypt = require("mongoose-encryption");
+//
 let userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
+
+//level 2
+//const SECRET = "SuperSecretKey"; //later on we will replace it with env variable
+//userSchema.plugin(encrypt, { secret: SECRET, encryptedFields: ["password"] });
+//
+//level 2.1 - it is better to hide our secret
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 let userModel = new mongoose.model("User", userSchema);
 module.exports = userModel;
